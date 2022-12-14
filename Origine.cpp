98effffdcb0e3891d60716e23f86b3adaec430be
DVT-DVD <dvtdavide@gmail.com>
2022-12-14 01:53:12 +0100

    Update Origine.cpp
    code refactor

--- Origine.cpp
+++ Origine.cpp
@@ -3,6 +3,7 @@
 #include<cassert>
 #include<cmath>
 
+#define MAGIC_NUMBER 19121998 // for random purpouse
 #define ask(test, x1, x2) {double _##test##[] = { x1, x2 };\
 					feedForward(&nn, _##test##); \
 					std::cout<<"Test " << x1 << "," << x2 << " = "  << getOutput(&nn) << std::endl; }
@@ -13,74 +14,83 @@
 #define NOT_OUTPUT(x) ( !IS_OUTPUT(x))
 #define ONE_ENDING(x, y) (IS_INPUT(x) || IS_OUTPUT(y))
 #define BOTH_ENDING(x, y) (IS_INPUT(x) && IS_OUTPUT(y))
+
+// you can map wathever values you want
 #define FALSE 0
 #define TRUE 1
 
+// XOR training dataset
 #define TRAINING_DATA 		double training_data[][3] = {\
 							{ TRUE, FALSE, TRUE},\
 							{ FALSE, TRUE, TRUE },\
 							{ TRUE, TRUE, FALSE },\
 							{ FALSE, FALSE, FALSE },\
 						};
+const unsigned DATASET_SIZE = 4; 
 
+// training parameter
+const double LEARNING_RATE = 0.0001f;
+const unsigned ITERATIONS = 1481;
 
-#define LEARNING_RATE  0.0001f
-
-const auto GENERAZIONI = 1481;
-const auto SIZE = 6;
-const auto DATASET_SIZE = 3;
-const auto input1 = 0;
-const auto input2 = 1;
-const auto BIAS = 2;
-const auto BIAS_VALUE = 0.0001f;
-const auto output1 = SIZE-1;
+// Network info
+const unsigned SIZE = 6;			// number of nodes in network (with input and output nodes)
+const unsigned input1 = 0;			// identify node of first input value
+const unsigned input2 = 1;			// identify node of second input value
+const unsigned BIAS = 2;			// identify bias node
+const unsigned output1 = SIZE - 1;  // identify output node
+const double BIAS_VALUE = 0.0001f;	// constant bias output
 
 
 typedef struct {
-	double connections[SIZE][SIZE];
-	double signals[SIZE];
+	double connections[SIZE][SIZE]; // contains all possible connection of the nodes
+	double signals[SIZE];           // signals emitted by each node
 } Network;
 
-const char *etichette[] = {
+const char *labels[] = {  // for output purpouses
 	"input1",
 	"input2",
 	"BIAS",
 	"h1",
 	"h2",	
 	"output1",
-	"h3",
-	"h4",
 };
 
-void init(Network* nn);
+// set all signals to 0  and all connections to a random value (i do not consider)
+// loop conections and connections in between nodes of the same layer
+void init(Network* nn); 
+// feed input to netwrk and generates output signal
 void feedForward(Network* nn, double inputs[]);
-double getOutput(Network* nn);
-void printConnections(Network * nn);
-bool exercise(Network* baby, double training_data[][3]);
-void correct(Network* baby, double error);
-double get_blame(Network* baby, int from, int to);
+// generic activation function for input and hidden nodes
 double act(double signal);
-//void rec_adjustment(Network* baby, int to, double error, bool* not_working);
+// acitivation function for output nodes
+double act_out(double signal);
+double getOutput(Network* nn);
+// print all connections of the network eg."from x to y: 0.00"
+void printConnections(Network * nn);
+// training function
+bool exercise(Network* baby, double training_data[][3]);
+// correct the wheights in the network(adjust connection value )
+void correct(Network* baby, double error);
+// get a percentage of how much a specific connection has contributed to get the error
+double get_blame(Network* baby, int from, int to);
+
 
 int main() {
 	
 	Network nn;
+	
 	init(&nn);
-	unsigned long long int perc = 0;
-	unsigned long long int old_perc = -1;
+	
 	unsigned long long int x = 0;
-	//for (unsigned long long int x = 0; x < GENERAZIONI; x++)
+	
+	// get the training data
 	TRAINING_DATA
+	
 	do{
-		// 100 : ? = 999999 : x
-		//perc = x * 100 / GENERAZIONI;
-		//if (perc != old_perc) {
-		//	std::cout << "Completamento: " << perc << "%" << std::endl;
-		//	old_perc = perc;
-		//}
 		x++;
-	} while (exercise(&nn, training_data));
-	std::cout << "Iterazioni totali necessarie: " << x << std::endl;
+	} while (exercise(&nn, training_data) && x < ITERATIONS);
+	std::cout << "Total Iterations: " << x << std::endl;
+	
 	ask(_10, TRUE, FALSE)
 	ask(_01, FALSE, TRUE)
 	ask(_00, FALSE, FALSE)
@@ -92,51 +102,49 @@ int main() {
 
 bool exercise(Network* baby, double training_data[][3])
 {
-	const auto TSET_SIZE = DATASET_SIZE;
-	const auto I1 = 0;
-	const auto I2 = 1;
-	const auto DO = 2;
+	const auto I1 = 0; // input1 in training_data
+	const auto I2 = 1; // input2 in training_data
+	const auto DO = 2; // desired output in training_data
+
 	bool exit = false;
-	for (int test = 0; test < TSET_SIZE; test++)
+
+	for (int test = 0; test < DATASET_SIZE; test++)
 	{
 		double error = 0.0f;
-		//do {
 		double test_case[] = { training_data[test][I1], training_data[test][I2] };
+
 		feedForward(baby, test_case);
+
 		error = training_data[test][DO] - getOutput(baby);
+		// if we get an error for the current test case 
+		// we need to adjust the connections
 		if (error != 0.0f) {
 			correct(baby, error);
 			exit = true;
 		}
-		//} while (ERROR_LIMIT);
 	}
 	return exit;
 }
 
 void correct(Network *baby, double error)
 {
-	// Aw = E * signal[from] * LR
-	
+	// correction formula for each weight (connections) 
+	// dw = Error * signal[from] * LEARNING_RATE	
 	for (int from = 0; from < SIZE; from++)
 	{
-		if (NOT_OUTPUT(from))
+		if (NOT_OUTPUT(from)) // filter all nodes that are not the output node
 		{
 			for (int to = 0; to < SIZE; to++)
 				if (ONE_ENDING(from, to) && NOT_INPUT(to) && !(IS_INPUT(from) && IS_OUTPUT(to)))
 				{
 					double blame = get_blame(baby, from, to);
-					// 100: blame = error : x
-					double blame2 = blame / error;
-					double adj = blame2 * act(baby->signals[from]);
+					blame = blame / error;
+					double adj = blame * act(baby->signals[from]);
 
-					if (adj == adj && adj != 0)
+					if (adj == adj && adj != 0) // adj==adj because it might be Nan
 						baby->connections[from][to] += adj * LEARNING_RATE;
-					else if(adj == 0 && baby->connections[from][to] == 0 && act(baby->signals[from]) != 0)
+					else if(adj == 0 && baby->connections[from][to] == 0 && act(baby->signals[from]) != 0)// condition to create a new connection
 						baby->connections[from][to] = LEARNING_RATE;
-					//if (baby->connections[from][to] == INFINITY)
-					//	baby->connections[from][to] = 1.0f;
-					//else if (act(baby->signals[from]) != 0.0f)
-					//	baby->connections[from][to] += LEARNING_RATE;
 				}
 		}
 	}
@@ -144,23 +152,25 @@ void correct(Network *baby, double error)
 
 double get_blame(Network * baby, int from, int to)
 {
-	double blame = 100 * (act(baby->signals[from]) * baby->connections[from][to]);
+	// like the feed_forward formula but for a single connection
+	double blame = 100 * (act(baby->signals[from]) * baby->connections[from][to]); 
 	if (blame == 0.0f)
-		return 0.0f;
+		return blame;
+	// check if the signal is not Nan and != 0
 	else if (baby->signals[to] == baby->signals[to] && baby->signals[to] != 0.0f)
-		if ((blame / act(baby->signals[to])) == INFINITY)
+		if ((blame / act(baby->signals[to])) == INFINITY) // it has all the blame so 100%...
 			return 100;
-		else if ((blame / act(baby->signals[to])) == -INFINITY)
+		else if ((blame / act(baby->signals[to])) == -INFINITY) // negative 100%
 			return -100;
 		else
-			return blame / act(baby->signals[to]);
+			return blame / act(baby->signals[to]); // calculate the percentage of blame
 
 	return 0.0f;
 }
 
 void init(Network* nn) {
-	std::srand(19121998);
-
+	std::srand(MAGIC_NUMBER);
+	// Initialize all signals and connections
 	for (int to = 0; to < SIZE; to++)
 	{
 		nn->signals[to] = 0.0f;
@@ -174,13 +184,21 @@ void init(Network* nn) {
 
 void feedForward(Network* nn, double inputs[])
 {
+	// reset signals in case of multiple executions
+	// like in the training session
 	for (int x = 0; x < SIZE; x++)
-		nn->signals[x] = 0.0f;
+		nn->signals[x] = 0.0f; 
 
+	// map inputs in the network
 	nn->signals[input1] = inputs[0];
 	nn->signals[input2] = inputs[1];
 	nn->signals[BIAS] = BIAS_VALUE;
 
+	// node activation formula:
+	// signals from previous node -> sig[from]
+	// wheight between previous and current node -> conn[from][to]
+	// sig[current] = sum sig[from] * conn[from][to]
+	// for all connections that sends signals to the current node
 	for (int to = 0; to < SIZE; to++)
 	{
 		if (NOT_INPUT(to))
@@ -189,32 +207,34 @@ void feedForward(Network* nn, double inputs[])
 				if(NOT_OUTPUT(from) && ONE_ENDING(from, to) && !BOTH_ENDING(from, to))
 					nn->signals[to] += act(nn->signals[from]) * nn->connections[from][to];
 		}
-
-
 	}
 }
 
 double act(double signal)
 {
-	//return tanh(signal);
+	// TBD: i do not have an actual activation function
+	// i just return the raw value calculated in the feed_forward function
 	return signal;
 }
 
 double act_out(double signal)
 {
-	//return tanh(signal);
+	// TBD: i noticed that to solve XOR problem
+	// the network will get similar values near 0 for the true case
+	// eg. 0 xor 1 = 0.08
+	// eg. 1 xor 0 = 0.08
+	// big values but different sign for the false case
+	// eg. 1 xor 1 = + 40.xxx
+	// eg. 0 xor 0 = - 40.xxx
 	double mid = double(TRUE - FALSE) / 2;
 	mid = TRUE - mid;
 
 	double lmid = mid - double(mid - FALSE)/2;
 	double hmid = TRUE - double(TRUE - mid)/2;
-	//std::cout << "Activation signal " << signal << std::endl;
-	//std::cout << "mid " << mid << std::endl;
-	//std::cout << "low mid " << lmid << std::endl;
-	//std::cout << "high mid " << hmid << std::endl;
+
 	return (signal  > lmid && signal < hmid )? TRUE: FALSE;
-	//  low mid high
-	//    lm   lh
+	//  low   mid    high
+	//    lmid   hmid
 }
 
 double getOutput(Network* nn)
@@ -223,11 +243,12 @@ double getOutput(Network* nn)
 }
 
 void printConnections(Network *nn) {
+	// print all connections of the network
 	for (int x = 0; x < SIZE; x++)
 	{
 		for (int y = 0; y < SIZE; y++)
-			std::cout << "from " << etichette[x] 
-			<< " to " << etichette[y] << " : " <<
+			std::cout << "from " << labels[x] 
+			<< " to " << labels[y] << " : " <<
 			nn->connections[x][y] <<std::endl;
 		
 	}
